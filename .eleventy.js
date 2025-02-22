ddb6f53a0bd83676a05ed128b702113b97bd5de9
ch0rch <georojas.gs@gmail.com>
2025-02-22 11:38:39 -0300

    blog

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
     // Copia los archivos estáticos
     eleventyConfig.addPassthroughCopy({ "assets": "assets" });
     
+    // Copia el archivo de configuración de admin
+    eleventyConfig.addPassthroughCopy({ "src/admin.yml": "admin/config.yml" });
+    
     // Filtro para fechas
     eleventyConfig.addFilter("dateFilter", function(date) {
         return new Date(date).toLocaleDateString('es-AR', {
@@ -10,17 +13,13 @@ module.exports = function(eleventyConfig) {
             day: 'numeric'
         });
     });
-    
+
     return {
         dir: {
             input: "src",
             output: "_site",
             includes: "_includes",
             layouts: "_layouts"
-        },
-        templateFormats: ["njk", "md", "html"],
-        htmlTemplateEngine: "njk",
-        markdownTemplateEngine: "njk",
-        pathPrefix: "/"
+        }
     };
 };
